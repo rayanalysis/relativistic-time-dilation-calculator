6a43e55fc022f67f2ff3c51c5e91a92863868d56
rayanalysis <ray.analysis@gmail.com>
2023-11-04 21:13:01 -0600

    Modifying increment loop to support small step factors.

--- gamma_equation.py
+++ gamma_equation.py
@@ -9,16 +9,16 @@ def calculate_gamma(v):
     return gamma
 
 # report spacecraft gamma
-v = 299777180  # spacecraft speed in m/s
-step_factor = 10
-for x in range(int(c - v / step_factor)):
+v = 299792422  # spacecraft speed in m/s
+step_factor = 0.1
+while v < c:
     try:
         gamma = calculate_gamma(v)
         percent_light = v/c
         print(f"At a speed of {v} m/s, {gamma} years pass on the planet for each year in the spacecraft.")
         print(f"The spacecraft is traveling at {percent_light}% light speed.")
         v += step_factor
-        time.sleep(0.3)
+        time.sleep(0.1)
     except:
         print('Final domain reached.')
         break
