962f3fedb23004ae0c9b63e01bf8e3bcc239cb79
rayanalysis <ray.analysis@gmail.com>
2023-11-04 20:58:25 -0600

    Adding speed increment loop.

--- gamma_equation.py
+++ gamma_equation.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 
 c = 299792458  # speed of light in m/s
@@ -8,8 +9,16 @@ def calculate_gamma(v):
     return gamma
 
 # report spacecraft gamma
-v = 299777100 # spacecraft speed in m/s
-gamma = calculate_gamma(v)
-percent_light = v/c
-print(f"At a speed of {v} m/s, {gamma} years pass on the planet for each year in the spacecraft.")
-print(f"The spacecraft is traveling at {percent_light}% light speed.")
+v = 299777180  # spacecraft speed in m/s
+step_factor = 10
+for x in range(int(c - v / step_factor)):
+    try:
+        gamma = calculate_gamma(v)
+        percent_light = v/c
+        print(f"At a speed of {v} m/s, {gamma} years pass on the planet for each year in the spacecraft.")
+        print(f"The spacecraft is traveling at {percent_light}% light speed.")
+        v += step_factor
+        time.sleep(0.3)
+    except:
+        print('Final domain reached.')
+        break
